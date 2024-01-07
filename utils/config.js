8d470d6c6649cc80f9ee42037a064e1cffbf633a
MarcusMathuram46 <marcus.mathuram7@gmail.com>
2024-01-08 01:19:11 +0530

    my

--- utils/config.js
+++ utils/config.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 const JWT_SECRET = process.env.JWT_SECRET;
-const EMAIL_USER = process.env.EMAIL_USER;
-const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
-const EMAIL_HOST = process.env.EMAIL_HOST;
-const EMAIL_PORT = process.env.EMAIL_PORT;
+// const EMAIL_USER = process.env.EMAIL_USER;
+// const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+// const EMAIL_HOST = process.env.EMAIL_HOST;
+// const EMAIL_PORT = process.env.EMAIL_PORT;
 
 
 module.exports = {
     MONGODB_URI,
     PORT,
-    JWT_SECRET,
-    EMAIL_USER,
-    EMAIL_PASSWORD,
-    EMAIL_HOST,
-    EMAIL_PORT
+    JWT_SECRET
+    // EMAIL_USER,
+    // EMAIL_PASSWORD,
+    // EMAIL_HOST,
+    // EMAIL_PORT
 }
