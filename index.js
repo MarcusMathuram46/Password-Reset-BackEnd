0429e8959a1e5ce8447b4bf25cb6ae381a5acadd
MarcusMathuram46 <marcus.mathuram7@gmail.com>
2024-01-08 12:30:35 +0530

    Last

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 console.log("Connecting to MongoDB...");
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI)
     .then(() => {
         info("Connected to MongoDB ...");
         app.listen(PORT, () => {
