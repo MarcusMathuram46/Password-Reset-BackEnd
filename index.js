6cf05652a181694e6a5e6516519ac9e587a67dd8
MarcusMathuram46 <marcus.mathuram7@gmail.com>
2024-01-08 13:54:25 +0530

    deploy render

--- index.js
+++ index.js
@@ -13,32 +13,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-console.log("Connecting to MongoDB...");
+console.log('Connecting to MongoDB...');
 
-mongoose.connect(MONGODB_URI)
-    .then(() => {
-        info("Connected to MongoDB...");
-        // Define routes
-        app.use('/user', userRouter);
-        app.use('/login', loginRouter);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    info('Connected to MongoDB...');
+    // Define routes
+    app.use('/user', userRouter);
+    app.use('/login', loginRouter);
 
-        // Error handling middleware
-        app.use((err, req, res, next) => {
-            err.statusCode = err.statusCode || 500;
-            res.status(err.statusCode).json({
-                error: {
-                    message: err.message,
-                },
-            });
-        });
-
-        // Start the server
-        app.listen(PORT, () => {
-            info(`Server is running at http://localhost:${PORT}`);
-        });
-    })
-    .catch((error) => {
-        err("Error connecting to MongoDB:", error);
-        // Terminate the process or handle the error accordingly
-        process.exit(1);
+    // Error handling middleware
+    app.use((err, req, res, next) => {
+      err.statusCode = err.statusCode || 500;
+      res.status(err.statusCode).json({
+        error: {
+          message: err.message,
+        },
+      });
     });
+
+    // Start the server
+    app.listen(PORT, () => {
+      info(`Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    err('Error connecting to MongoDB:', error);
+    // Terminate the process or handle the error accordingly
+    process.exit(1);
+  });
+
+module.exports = app;
